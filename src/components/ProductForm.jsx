202504f1b468366c43add69eb490b9367ddb726f
hoangnm-ndm <hoangnm.ndm@gmail.com>
2024-06-22 13:06:05 +0700

    Cloudinary

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,21 +1,24 @@
-import { zodResolver } from "@hookform/resolvers/zod";
 import React, { useContext, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import productSchema from "../schemaValid/productSchema";
 import { useNavigate, useParams } from "react-router-dom";
 import { ProductContext } from "../contexts/ProductContext";
 import instance from "../axios";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-console.log(import.meta.env.VITE_UPLOAD_PRESET);
-
-// const { CLOUD_NAME, API_KEY, API_SECRET, UPLOAD_PRESET } = process.env;
-// console.log(process.env);
+const { VITE_CLOUD_NAME, VITE_UPLOAD_PRESET } = import.meta.env;
 
 const ProductForm = () => {
 	const { id } = useParams();
 	const { dispatch } = useContext(ProductContext);
 	const nav = useNavigate();
-	const [thumbnail, setThumbnail] = useState(null);
+	const [thumbnailUrl, setThumbnailUrl] = useState(null);
+
+	// State để lưu trữ lựa chọn của người dùng
+	const [thumbnailOption, setThumbnailOption] = useState("keep");
+
+	// State lưu trữ link ảnh từ bên ngoài
+	const [linkOnlineImage, setLinkOnlineImage] = useState("");
 
 	const {
 		register,
@@ -26,45 +29,59 @@ const ProductForm = () => {
 		resolver: zodResolver(productSchema),
 	});
 
-	if (id) {
-		useEffect(() => {
+	useEffect(() => {
+		if (id) {
 			(async () => {
 				const { data } = await instance.get(`/products/${id}`);
+				console.log(data);
 				reset(data);
-				setThumbnail(data.thumbnail);
+				setThumbnailUrl(data.thumbnail);
 			})();
-		}, [id, reset]);
-	}
+		}
+	}, [id, reset]);
 
 	const uploadImage = async (file) => {
 		const formData = new FormData();
 		formData.append("file", file);
-		formData.append("upload_preset", UPLOAD_PRESET);
+		formData.append("upload_preset", VITE_UPLOAD_PRESET);
 
-		const response = await fetch(`https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`, {
+		const response = await fetch(`https://api.cloudinary.com/v1_1/${VITE_CLOUD_NAME}/image/upload`, {
 			method: "POST",
 			body: formData,
 		});
 		const data = await response.json();
+		console.log(data);
 		return data.secure_url;
 	};
 
 	const onSubmit = async (product) => {
 		try {
-			let thumbnailUrl = thumbnail;
-			if (product.thumbnail && product.thumbnail[0]) {
-				thumbnailUrl = await uploadImage(product.thumbnail[0]);
+			let updatedProduct = { ...product };
+			// Kiểm tra lựa chọn của người dùng và xử lý tương ứng
+			switch (thumbnailOption) {
+				case "upload":
+					// Xử lý upload ảnh từ local
+					if (product.thumbnail && product.thumbnail[0]) {
+						const thumbnailUrl = await uploadImage(product.thumbnail[0]);
+						updatedProduct = { ...updatedProduct, thumbnail: thumbnailUrl };
+					}
+					break;
+				default:
+				// Giữ nguyên ảnh cũ khi không thay đổi
+				// Hoặc mặc định khi người dùng chọn "link online"
 			}
-			const updatedProduct = { ...product, thumbnail: thumbnailUrl };
 
 			if (id) {
-				await instance.put(`/products/${id}`, updatedProduct);
+				const { data } = await instance.patch(`/products/${id}`, updatedProduct);
 				dispatch({ type: "UPDATE_PRODUCT", payload: { id, product: updatedProduct } });
+				console.log(data);
 			} else {
 				const { data } = await instance.post("/products", updatedProduct);
 				dispatch({ type: "ADD_PRODUCT", payload: data });
+				console.log(data);
 			}
-			if (confirm("Redirect to admin?")) nav("/admin");
+
+			nav("/admin");
 		} catch (error) {
 			console.error(error);
 		}
@@ -72,7 +89,7 @@ const ProductForm = () => {
 
 	return (
 		<div>
-			<form onSubmit={handleSubmit((data) => onSubmit({ ...data, id }))}>
+			<form onSubmit={handleSubmit(onSubmit)}>
 				<h1>{id ? "Edit" : "Add"} product</h1>
 				<div className="mb-3">
 					<label htmlFor="title" className="form-label">
@@ -100,15 +117,58 @@ const ProductForm = () => {
 					<input type="text" className="form-control" id="description" {...register("description")} />
 					{errors.description?.message && <p className="text-danger">{errors.description?.message}</p>}
 				</div>
-				<div className="mb-3">
+				{/* <div className="mb-3">
 					<label htmlFor="thumbnail" className="form-label">
 						Thumbnail
 					</label>
 					<input type="file" className="form-control" id="thumbnail" {...register("thumbnail")} />
-					{thumbnail && (
-						<img src={thumbnail} alt="Product Thumbnail" style={{ maxWidth: "200px", marginTop: "10px" }} />
+					{errors.thumbnail?.message && <p className="text-danger">{errors.thumbnail?.message}</p>}
+					{thumbnailUrl && (
+						<img src={thumbnailUrl} alt="Product Thumbnail" style={{ maxWidth: "200px", marginTop: "10px" }} />
 					)}
+				</div> */}
+
+				<div className="mb-3">
+					<label htmlFor="thumbnailOption" className="form-label">
+						Choose Thumbnail Option
+					</label>
+					<select
+						className="form-control"
+						id="thumbnailOption"
+						value={thumbnailOption}
+						onChange={(e) => setThumbnailOption(e.target.value)}
+					>
+						<option value="keep">Keep Current Thumbnail</option>
+						<option value="link">Add Thumbnail from Link</option>
+						<option value="upload">Upload Thumbnail from Local</option>
+					</select>
 				</div>
+
+				{thumbnailOption === "link" && (
+					<div className="mb-3">
+						<label htmlFor="thumbnail" className="form-label">
+							Thumbnail
+						</label>
+						<input type="text" className="form-control" id="thumbnail" {...register("thumbnail")} />
+						{errors.thumbnail?.message && <p className="text-danger">{errors.thumbnail?.message}</p>}
+						{thumbnailUrl && (
+							<img src={thumbnailUrl} alt="Product Thumbnail" style={{ maxWidth: "200px", marginTop: "10px" }} />
+						)}
+					</div>
+				)}
+
+				{thumbnailOption === "upload" && (
+					<div className="mb-3">
+						<label htmlFor="thumbnail" className="form-label">
+							Thumbnail
+						</label>
+						<input type="file" className="form-control" id="thumbnail" {...register("thumbnail")} />
+						{errors.thumbnail?.message && <p className="text-danger">{errors.thumbnail?.message}</p>}
+						{thumbnailUrl && (
+							<img src={thumbnailUrl} alt="Product Thumbnail" style={{ maxWidth: "200px", marginTop: "10px" }} />
+						)}
+					</div>
+				)}
 				<div className="mb-3">
 					<button className="btn btn-primary w-100" type="submit">
 						{id ? "Edit" : "Add"} product
